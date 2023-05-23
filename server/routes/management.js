3dee99c8c5ffdde2a366f87dfd2f3e656cf2bdad
Shall-we-play-a-game <mr.imk.khan@gmail.com>
2023-05-23 23:53:57 +0530

    init commit

---
+++ server/routes/management.js
@@ -0,0 +1,7 @@
+import express from "express";
+import { getAdmin, getUserPerformance } from "../controllers/management.js";
+
+const router = express.Router();
+router.get("/admin", getAdmin);
+router.get("/performance/:id", getUserPerformance);
+export default router;
